62c475bd5abc61eb9cd29647e4d982e233cc3632
lhmn <maingan.19042001@gmail.com>
2022-05-11 17:51:27 +0700

    Initial commit

---
+++ app/src/main/java/hcmute/spkt/mssv19110245/myfoody/MainActivity.java
@@ -0,0 +1,14 @@
+package hcmute.spkt.mssv19110245.myfoody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
